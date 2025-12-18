962b28b30963ed93d94a98fc67381753af23fdbc
lulubin <452936616@qq.com>
2025-12-18 13:49:56 +0800

    修复bug

--- pscws4.php
+++ pscws4.php
@@ -1,6 +1,8 @@
 <?php
 namespace lulubin\pscws4;
 
+use lulubin\pscws4\pscws4\PSCWS4BASE;
+
 /**
  * IP 地理位置查询类
  */
@@ -11,11 +13,12 @@ class pscws4
      */
     public function split($text)
     {
-        require_once './pscws4/PSCWS4.php';
-        $pcws = new PSCWS4();
+        
+        $currentDir = __DIR__;
+        $pcws = new PSCWS4BASE();
         $pcws->set_charset('utf-8');
-        $pcws->set_dict('./dict.utf8.xdb');
-        $pcws->set_rule('./pscws4/etc/rules.ini');
+        $pcws->set_dict($currentDir.'/dict.utf8.xdb');
+        $pcws->set_rule($currentDir.'/pscws4/etc/rules.ini');
         $pcws->send_text("印花冰丝凉");
         $article = [];
         while ($some = $pcws->get_result())
